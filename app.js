456a57a930fd2b3caf32615faa996604ba4e351f
krati1711 <kratimathur142@gmail.com>
2020-06-03 19:47:27 +0530

    Pug Finished

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
+
 const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
 const app = express();
-const adminRoutes = require('./routes/admin');
+
+app.set('view engine','pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    //res.status(404).send('<h1> Page not found </h1>');
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404',{pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
-// alternative for above code
-//const server = http.createServer(app);
-//server.listen(3000);
